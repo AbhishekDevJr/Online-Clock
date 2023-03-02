86daa88d60af863ce4f53ab81abe8ac5e23ca87b
Abhishek Choudhari <abhishek007coc@gmail.com>
2023-03-03 00:57:04 +0530

    Post deployment changes: Added the logic to keep user's device awake while user being inactive, some ui changes

--- clock-app/src/Main.js
+++ clock-app/src/Main.js
@@ -13,6 +13,8 @@ function Main(){
         console.log('Built by Abhishek Choudhari');
     }, []);
 
+    //Logic to Display Current Time
+
     function setCurrentTime(){
         setInterval(() => {
             let newTime = new Date();
@@ -24,6 +26,34 @@ function Main(){
 
     setCurrentTime();
 
+    //Logic to keep user's device awake(Not get into sleep/screensaver mode) while the user is idle.
+
+    function isScreenLockSupported() {
+        return ('wakeLock' in navigator);
+    }
+
+    async function getScreenLock() {
+    if(isScreenLockSupported()){
+        let screenLock;
+        try {
+            screenLock = await navigator.wakeLock.request('screen');
+            // console.log('Try Block');
+            document.addEventListener('visibilitychange', async () => {
+            if (screenLock !== null && document.visibilityState === 'visible') {
+                screenLock = await navigator.wakeLock.request('screen');
+            }
+            });
+        } catch(err) {
+            // console.log(err.name, err.message);
+        }
+        return screenLock;
+    }
+    }
+
+    getScreenLock();
+
+      
+       
     return(
         <div className = "main">
             <div className = "clock-container">
